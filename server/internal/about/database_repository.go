5ccb9770e32b4013ef7e765170c84c3c43cf5ad0
Thiago Sousa <thiago@thiagoodev.com.br>
2026-05-17 14:40:46 -0300

    about repo error

--- server/internal/about/database_repository.go
+++ server/internal/about/database_repository.go
@@ -2,19 +2,46 @@ package about
 
 import (
 	"context"
-	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/cthiagoodev/thiagoodev-portfolio/server/internal/common"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DatabaseRepository struct {
-	db *sql.DB
+	pool *pgxpool.Pool
 }
 
-func NewDatabaseRepository(db *sql.DB) *DatabaseRepository {
-	return &DatabaseRepository{db: db}
+func NewDatabaseRepository(pool *pgxpool.Pool) *DatabaseRepository {
+	return &DatabaseRepository{pool}
 }
 
 func (r *DatabaseRepository) Find(ctx context.Context) (About, error) {
-	return About{}, common.ErrNotFound
+	query := "SELECT uuid, name, bio, photo, curriculum, linkedin, github, city, state FROM about LIMIT 1"
+	row := r.pool.QueryRow(ctx, query)
+
+	about := About{}
+	err := row.Scan(
+		&about.Uuid,
+		&about.Name,
+		&about.Bio,
+		&about.Photo,
+		&about.Curriculum,
+		&about.Linkedin,
+		&about.GitHub,
+		&about.City,
+		&about.State,
+	)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return About{}, common.ErrNotFound
+	}
+
+	if err != nil {
+		return About{}, fmt.Errorf("about not found %w", err)
+	}
+
+	return about, nil
 }
---
+++ server/internal/common/db.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDb(url string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
