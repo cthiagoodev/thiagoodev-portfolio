474407684d4f4e558243a0246e70b5c0edfa83c7
Thiago Sousa <thiagoodev@MacBook-Air-de-Thiago.local>
2026-06-07 18:56:00 -0300

    errors handle

--- server/internal/common/handle_error.go
+++ server/internal/common/handle_error.go
@@ -2,17 +2,29 @@ package common
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
-func HandleError(w http.ResponseWriter, err string, statusCode int) {
+func HandleError(w http.ResponseWriter, err error) {
 	w.Header().Set("Content-Type", "application/json")
+
+	statusCode := http.StatusInternalServerError
+
+	switch {
+	case errors.Is(err, ErrNotFound):
+		statusCode = http.StatusNotFound
+	case errors.Is(err, ErrInvalidData):
+		statusCode = http.StatusBadRequest
+	case errors.Is(err, ErrConnection):
+		statusCode = http.StatusServiceUnavailable
+	default:
+		statusCode = http.StatusInternalServerError
+	}
+
 	w.WriteHeader(statusCode)
-
-	encoder := json.NewEncoder(w)
-
-	encoder.Encode(map[string]any{
+	json.NewEncoder(w).Encode(map[string]any{
 		"status": statusCode,
-		"error":  err,
+		"error":  err.Error(),
 	})
 }
