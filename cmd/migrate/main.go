e4a39dbe0f408ef1c609dc3a76f56f8f8b53b176
Thiago Sousa <thiagoodev@MacBook-Air-de-Thiago.local>
2026-06-21 18:20:11 -0300

    create migration scope

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
 )
 
@@ -35,4 +41,38 @@ func Up() {
 
 func Down() {
 
+}
+
+func getDbInstance() *sql.DB {
+	url := os.Getenv("DATABASE_URL")
+	conn, err := sql.Open("pgx", url)
+
+	if err != nil {
+		log.Fatal("Error on connection database")
+		return nil
+	}
+
+	return conn
+}
+
+func getDbDriver(db *sql.DB) *database.Driver {
+	dvr, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		log.Fatal("Error on connection database")
+		return nil
+	}
+
+	return &dvr
+}
+
+func getMigrationInstance(dvr database.Driver) *migrate.Migrate {
+	mgt, err := migrate.NewWithDatabaseInstance("file://migrations", "pgx", dvr)
+
+	if err != nil {
+		log.Fatal("Error on connection database")
+		return nil
+	}
+
+	return mgt
 }
