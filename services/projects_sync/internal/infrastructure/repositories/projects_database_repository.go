59dbdc6e78fadbf6147ab242fc8be92f65ad490c
Thiago Sousa <thiago@thiagoodev.com.br>
2026-08-30 20:50:08 -0300

    refactor sync

--- services/projects_sync/internal/infrastructure/repositories/projects_database_repository.go
+++ services/projects_sync/internal/infrastructure/repositories/projects_database_repository.go
@@ -1,21 +1,4 @@
 package repositories
 
-import "github.com/cthiagoodev/thiagoodev-portfolio/services/projects_sync/internal/domain/entities"
-
 type ProjectsDatabaseRepository struct {
-}
-
-func (p *ProjectsDatabaseRepository) GetAll() ([]entities.Project, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (p *ProjectsDatabaseRepository) CreateOrUpdate(project entities.Project) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (p *ProjectsDatabaseRepository) Delete(uuid string) error {
-	//TODO implement me
-	panic("implement me")
 }
