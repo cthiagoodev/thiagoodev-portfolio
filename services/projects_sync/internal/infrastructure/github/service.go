88b0d556f8d9ae50f2fa5c9714b20324c2964496
Thiago Sousa <thiago@thiagoodev.com.br>
2026-09-06 19:59:06 -0300

    create github service

--- services/projects_sync/internal/infrastructure/github/service.go
+++ services/projects_sync/internal/infrastructure/github/service.go
@@ -1,11 +1,56 @@
 package github
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
 type GithubService interface {
 	FetchRepositories() ([]Project, error)
 }
 
-type GithubServiceImpl struct{}
+type GithubServiceImpl struct {
+	client  *http.Client
+	baseURL *url.URL
+}
+
+func NewGithubServiceImpl(client *http.Client, baseURL *url.URL) *GithubServiceImpl {
+	return &GithubServiceImpl{
+		client,
+		baseURL,
+	}
+}
 
 func (s *GithubServiceImpl) FetchRepositories() ([]Project, error) {
-	panic("implement me")
+	path := s.baseURL.String() + "/users/cthiagoodev/repos"
+	response, err := s.client.Get(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+
+	bytes, rErr := io.ReadAll(response.Body)
+
+	if rErr != nil {
+		return nil, rErr
+	}
+
+	projects := make([]Project, 0)
+
+	jErr := json.Unmarshal(bytes, &projects)
+
+	if jErr != nil {
+		return nil, jErr
+	}
+
+	return projects, nil
 }
